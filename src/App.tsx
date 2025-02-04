32124cbe0c9af10d749398f3bd70ceeebc21b7a1
kemboi590 <bkemboi590@gmail.com>
2025-02-04 14:12:41 +0300

    removed uncesesary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
-// import './App.css'
-
 import { Toaster } from "sonner"
-import ForgotPasword from "./pages/Auth/ForgotReset/ForgotPasword"
-import { ResetPassword } from "./pages/Auth/ForgotReset/ResetPassword"
-import { Login } from "./pages/Auth/login/Login"
-import Register from "./pages/Auth/register/Register"
 
 function App() {
   return (
