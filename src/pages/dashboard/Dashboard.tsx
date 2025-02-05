5d95f7cbd18cf27212154b4f4a8762c2a84c8eca
kemboi590 <bkemboi590@gmail.com>
2025-02-05 13:24:10 +0300

    added the dashboard

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,22 @@
+import { Outlet } from "react-router-dom"
+import Navbar from "../../components/Navbar"
+import Drawer from "./aside/Drawer"
+
+export const Dashboard = () => {
+    return (
+        <>
+            <Navbar />
+            <div className="flex">
+                <aside className="lg:w-64 h-screen bg-white shadow-lg">
+                    <Drawer />
+                </aside>
+                <main className="flex-1 p-4 overflow-auto">
+                    <Outlet />
+                </main>
+            </div>
+        </>
+
+    )
+}
+
+export default Dashboard
